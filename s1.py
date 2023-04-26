c418da0addaf483d5fd6ee710095c8e7c12441b7
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-26 18:58:08 +0330

    sadeghi

---
+++ s1.py
@@ -0,0 +1,92 @@
+# a = 5
+# b = 6
+
+# if a > b:
+#     print("a is greater than b")
+#     print("somthing else")
+
+# print("outside")
+
+# string1 = 'c'
+# string2 = 'b'
+
+# print(ord(string1))
+# print(ord(string2))
+
+# if string1 > string2:
+#     print("string1 is greater than string2")
+
+# name1 = 'amir'
+# name2 = 'amin'
+
+# if name1 > name2:
+#     print("name1 is greater than name2")
+
+# >   <     >=    <=     ==    !=
+
+
+# name1 = "tara"
+# name2 = "sara"
+
+# if name1 == name2:
+#     print("name1 and name2 are equal")
+# else:
+#     print("name1 and name2 are different")
+
+
+# number1 = int(input('enter a number: '))
+# number2 = int(input('enter a number: '))
+
+# if number1 == number2:
+#     print(f"{number1} = {number2}")
+# elif number1 > number2:
+#     print(f"{number1} > {number2}")
+# else:
+#     print(f"{number1} < {number2}")
+
+# and      or    not
+
+# a = 12
+# b = 13
+# c = 10
+
+# if a > b and a > c:
+#     print("a is greater than b and c")
+# if a > b or a > c:
+#     print("a is greater than b or c")
+
+# Boolean    True     False
+
+# x = True
+# print(type(x))
+
+# if True:
+#     print("ok")
+
+# if False:
+#     print("not ok")
+
+# print(not(True))
+# print(not(False))
+
+# a = b = 10
+
+
+# print("a = ",a)
+# print("b = ",b)
+
+# a = b = 10
+# c = a == b
+# print(c)
+
+# a = 10
+# b = 12
+# c = a == b
+# print(c)
+
+
+user_name = input("What's your name: ").lower()
+if user_name == "reza" or user_name == "sara":
+    print("you have a nice name.")
+else:
+    print("your name is ok.")
