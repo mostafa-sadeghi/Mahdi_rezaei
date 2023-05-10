c175a746671490f942c6caa4ec60a7aa21e030fd
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-05-10 18:56:49 +0330

    sadeghi

---
+++ s3.py
@@ -0,0 +1,59 @@
+
+#1# What does this code output?
+
+# x = 10 + 6 / 2
+# x = x + 1
+# print(x)
+
+
+# 13
+# 8
+# 14
+# Nothing
+# 9
+
+#######################################################################
+
+# 2. 
+'''
+What does this code output?
+
+x = input("Enter a value:")
+print(x / 2)
+
+An error because the value was not converted to a number.
+The value the user entered.
+The value the user entered divided by two.
+10
+0
+'''
+
+######################################################################
+# 3. 
+'''
+What does this code output?
+
+print("Have a "great" day!")
+
+
+Have a "great" day!
+"Have a "great" day!"
+Nothing, the double quotes make for a syntax error.
+Have a great day!
+'''
+
+######################################################################
+# 4. 
+'''
+print("Save in c:\new folder")
+
+
+Save in c:
+ew folder
+Nothing, the escape code makes for a syntax error.
+Save in c:ew folder
+Save in c:\new folder
+'''
+
+
+# برنامه ای بنویس که نام کاربر را از ورودی دریافت نماید و کاراکترهای آن را یکی در میان نمایش دهد
